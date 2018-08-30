92092b614d9d2c9457f692503d51ce0c7ecc00ce
mersed <kahrimanovic_mersed@hotmail.com>
2018-08-30 05:13:31 +0200

    Adding bitwise and operation

--- main.c
+++ main.c
@@ -4,23 +4,55 @@
 
 
 int main(int argc, char **argv) {
-	int input;
-	char *binary_num = malloc(65 * sizeof(char));
-	char *binary_num_1_compl = malloc(65 * sizeof(char));
+	int input1, input2, bitwise_and;
+	char *input1_bin = malloc(65 * sizeof(char));
+	char *input1_bin_1c = malloc(65 * sizeof(char));
+	char *input1_bin_2c = malloc(65 * sizeof(char));
+	char *input2_bin = malloc(65 * sizeof(char));
+	char *input2_bin_1c = malloc(65 * sizeof(char));
+	char *input2_bin_2c = malloc(65 * sizeof(char));
+	char *bitwise_and_bin = malloc(65 * sizeof(char));
 
-	printf("Insert integer (decimal number) in order to apply binary operations: ");
-	scanf("%d", &input);
+	printf("Insert first integer (decimal number): ");
+	scanf("%d", &input1);
+	printf("Insert second integer (decimal number): ");
+	scanf("%d", &input2);
 
-	dec_to_binary(input, binary_num);
-	dec_to_binary(~input, binary_num_1_compl);
+	dec_to_binary(input1, input1_bin);
+	dec_to_binary(~input1, input1_bin_1c);
+	dec_to_binary(~input1+1, input1_bin_2c);
 
+	dec_to_binary(input2, input2_bin);
+	dec_to_binary(~input2, input2_bin_1c);
+	dec_to_binary(~input2+1, input2_bin_2c);
 
-	printf("Binary representation of number %d is %s\n", input, binary_num);
+	printf("Binary representation of number %d is %s\n", input1, input1_bin);
 	printf("--------------------------------------------------------------------\n");
-	printf("First complement of %s is %s\n", binary_num, binary_num_1_compl);
+	printf("1'st complement of %s is %s\n", input1_bin, input1_bin_1c);
+	printf("2'nd complement of %s is %s\n\n", input1_bin, input1_bin_2c);
 
 
-	free(binary_num);
-	free(binary_num_1_compl);
+	printf("Binary representation of number %d is %s\n", input2, input2_bin);
+	printf("--------------------------------------------------------------------\n");
+	printf("1'st complement of %s is %s\n", input2_bin, input2_bin_1c);
+	printf("2'nd complement of %s is %s\n\n", input2_bin, input2_bin_2c);
+
+	bitwise_and = input1 & input2;
+	dec_to_binary(bitwise_and, bitwise_and_bin);
+	printf("Bitwise AND operation(&)\n");
+	printf("--------------------------------------------------------------------\n");
+	printf("%s\n", input1_bin);
+	printf("&\n");
+	printf("%s\n", input2_bin);
+	printf("--------------------------------\n");
+	printf("%s\n", bitwise_and_bin);
+
+	free(input1_bin);
+	free(input1_bin_1c);
+	free(input1_bin_2c);
+	free(input2_bin);
+	free(input2_bin_1c);
+	free(input2_bin_2c);
+	free(bitwise_and_bin);
 	return 0;
 }
