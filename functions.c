9286c42c7b4849b56d4fb19ee233657ac6761f6a
mersed <kahrimanovic_mersed@hotmail.com>
2018-08-30 03:32:59 +0200

    Implementing one's complement.

--- functions.c
+++ functions.c
@@ -5,14 +5,10 @@
  * which are going to be stored in string (array of chars).
  */
 void dec_to_binary(int n, char *binary_number) {
-	
-	int i=0;
-	int j;
-
-	while(n>0) {
-		binary_number[i] = n%2 ? '1':'0';
-		n = n/2;
-		i++;
+	int i;
+	for(i=0; i<32; i++) {
+		binary_number[i] = n & 0x80000000 ? '1':'0';
+		n = n << 1;
 	}
 
 	binary_number[i] = '\0';
--- main.c
+++ main.c
@@ -4,11 +4,23 @@
 
 
 int main(int argc, char **argv) {
-	char *binary_number = malloc(65 * sizeof(char));
+	int input;
+	char *binary_num = malloc(65 * sizeof(char));
+	char *binary_num_1_compl = malloc(65 * sizeof(char));
 
-	dec_to_binary(5, binary_number);
-	printf("Ovo je nas binarni broj: %s\n", binary_number );
+	printf("Insert integer (decimal number) in order to apply binary operations: ");
+	scanf("%d", &input);
+
+	dec_to_binary(input, binary_num);
+	dec_to_binary(~input, binary_num_1_compl);
 
 
-	free(binary_number);
+	printf("Binary representation of number %d is %s\n", input, binary_num);
+	printf("--------------------------------------------------------------------\n");
+	printf("First complement of %s is %s\n", binary_num, binary_num_1_compl);
+
+
+	free(binary_num);
+	free(binary_num_1_compl);
+	return 0;
 }
