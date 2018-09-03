570d9fb26806ec3f9c5c975b4b6a50b46dd5c791
mersed <kahrimanovic_mersed@hotmail.com>
2018-09-03 20:11:28 +0200

    Adding left and right shift bitwise operators.

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include "functions.h"
 
-#define SYSTEM_PROC 64
+#define BYTE 8
 
 int main(int argc, char **argv) {
-	int input1, input2, bitwise_and, bitwise_or, bitwise_xor;
-	char input1_bin[SYSTEM_PROC+1], input1_bin_1c[SYSTEM_PROC+1], input1_bin_2c[SYSTEM_PROC+1], input2_bin[SYSTEM_PROC+1];
-	char *input2_bin_1c = malloc(65);
-	char *input2_bin_2c = malloc(65);
-	char *bitwise_and_bin = malloc(65);
-	char *bitwise_or_bin = malloc(65);
-	char *bitwise_xor_bin = malloc(65);
+	const int int_bit_length = sizeof(int) * BYTE;
+	const int str_length = int_bit_length+1;
+
+	int input1, input2, bitwise_and, bitwise_or, bitwise_xor, bitwise_shift_left1, bitwise_shift_left2, bitwise_shift_left3,
+		bitwise_shift_right1, bitwise_shift_right2, bitwise_shift_right3;
+	char input1_bin[str_length], input1_bin_1c[str_length], input1_bin_2c[str_length];
+	char input2_bin[str_length], input2_bin_1c[str_length], input2_bin_2c[str_length];
+	char bitwise_and_bin[str_length], bitwise_or_bin[str_length], bitwise_xor_bin[str_length], bitwise_shift_left1_bin[str_length],
+		 bitwise_shift_left2_bin[str_length], bitwise_shift_left3_bin[str_length], bitwise_shift_right1_bin[str_length], 
+		 bitwise_shift_right2_bin[str_length], bitwise_shift_right3_bin[str_length];
 
 	printf("Insert first integer (decimal number): ");
 	scanf("%d", &input1);
@@ -71,13 +74,32 @@ int main(int argc, char **argv) {
 	printf("--------------------------------\n");
 	printf("%s\n\n", bitwise_xor_bin);
 
+	// Shift left operation
+	bitwise_shift_left1 = input1 << 1;
+	bitwise_shift_left2 = input1 << 2;
+	bitwise_shift_left3 = input1 << 3;
+	dec_to_binary(bitwise_shift_left1, bitwise_shift_left1_bin);
+	dec_to_binary(bitwise_shift_left2, bitwise_shift_left2_bin);
+	dec_to_binary(bitwise_shift_left3, bitwise_shift_left3_bin);
+	printf("Bitwise SHIFT LEFT operation(<<)\n");
+	printf("--------------------------------------------------------------------\n");
+	printf("%s << 1 = %s\n", input1_bin, bitwise_shift_left1_bin);
+	printf("%s << 2 = %s\n", input1_bin, bitwise_shift_left2_bin);
+	printf("%s << 3 = %s\n\n", input1_bin, bitwise_shift_left3_bin);
+
+	// Shift right operation
+	bitwise_shift_right1 = input2 >> 1;
+	bitwise_shift_right2 = input2 >> 2;
+	bitwise_shift_right3 = input2 >> 3;
+	dec_to_binary(bitwise_shift_right1, bitwise_shift_right1_bin);
+	dec_to_binary(bitwise_shift_right2, bitwise_shift_right2_bin);
+	dec_to_binary(bitwise_shift_right3, bitwise_shift_right3_bin);
+	printf("Bitwise SHIFT RIGHT operation(>>)\n");
+	printf("--------------------------------------------------------------------\n");
+	printf("%s >> 1 = %s\n", input2_bin, bitwise_shift_right1_bin);
+	printf("%s >> 2 = %s\n", input2_bin, bitwise_shift_right2_bin);
+	printf("%s >> 3 = %s\n\n", input2_bin, bitwise_shift_right3_bin);
 
 
-
-	free(input2_bin_1c);
-	free(input2_bin_2c);
-	free(bitwise_and_bin);
-	free(bitwise_or_bin);
-	free(bitwise_xor_bin);
 	return 0;
 }
